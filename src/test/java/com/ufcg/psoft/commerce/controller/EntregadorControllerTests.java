bfac608d53de1dd9042ea79a87f0ca9225e8dcef
Mateus <130506942+mateusmf4@users.noreply.github.com>
2025-02-26 14:41:09 -0300

    refactor: mover testes de entregador

--- src/test/java/com/ufcg/psoft/commerce/controller/EntregadorControllerTests.java
+++ src/test/java/com/ufcg/psoft/commerce/controller/EntregadorControllerTests.java
@@ -1,4 +1,4 @@
-package com.ufcg.psoft.commerce.controller.Entregador;
+package com.ufcg.psoft.commerce.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
