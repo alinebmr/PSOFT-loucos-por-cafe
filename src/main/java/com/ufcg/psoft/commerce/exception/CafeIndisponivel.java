3dd4f6aa2d07b99a4885c9285d82b893b29f4007
FLuiz22 <filipeluiz39@gmail.com>
2025-02-28 17:49:35 -0300

    Feat: Cafe Indisponível exception added

---
+++ src/main/java/com/ufcg/psoft/commerce/exception/CafeIndisponivel.java
@@ -0,0 +1,7 @@
+package com.ufcg.psoft.commerce.exception;
+
+public class CafeIndisponivel extends CommerceException {
+    public CafeIndisponivel() {
+        super("Cafe indisponivel!");
+    }
+}
