21491ca4e08d5531072bd7fc2bbb7793d2d7aa51
FLuiz22 <filipeluiz39@gmail.com>
2025-02-28 17:21:50 -0300

    Feat: FornecedorInvalido Exception added

---
+++ src/main/java/com/ufcg/psoft/commerce/exception/FornecedorInvalido.java
@@ -0,0 +1,7 @@
+package com.ufcg.psoft.commerce.exception;
+
+public class FornecedorInvalido extends CommerceException {
+    public FornecedorInvalido() {
+        super("Fornecedor invalido!");
+    }
+}
